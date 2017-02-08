f88d80461bd94a4e7ac12fd6811ef65a9accc301
Matthew Weidman <weidman.matthew@gmail.com>
2017-02-07 20:36:44 -0700

    first commit

---
+++ mnist_parse.py
@@ -0,0 +1,16 @@
+import struct
+import numpy as np
+import scipy.misc
+
+def getLabels():
+    with open('train-labels-idx3-ubyte', 'rb') as f:
+        magic, num = struct.unpack(">II", f.read(8))
+        labels = np.fromfile(f, dtype=np.uint8)
+        return labels
+
+def getImages():
+    with open('train-images-idx3-ubyte', 'rb') as f:
+        magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
+        imgs = np.fromfile(f, dtype=np.uint8).reshape(num, rows, cols)
+        return imgs
+
